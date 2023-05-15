5802fa79105236af2106c1c03fe272ea2002d8e2
Ben Harruff <harruff@pm.me>
2023-05-15 01:01:33 -0400

    Added license and edited about blurb

--- src/messages.rs
+++ src/messages.rs
@@ -4,8 +4,21 @@ fn line_break() {
     println!("{}", "-".repeat(80));
 }
 
+fn newline() {
+    println!("");
+}
+
 pub fn about() {
     line_break();
-    println!("Spiffo is a command-line interface built to manage running a Project Zomboid server.");
+    println!("Spiffo is a command-line interface meant to manage a dedicated Project Zomboid");
+    println!("multiplayer server with support for Steam Workshop mods, backups, updates, etc.");
+    newline();
+    println!("Currently, Spiffo is a personal project and is not yet a functioning prototype.");
+    println!("Nonetheless, I greatly appreciate the use of my tool all the same.");
+    newline();
+    println!("Discussions are welcome and encouraged:");
+    println!("https://github.com/junkben/spiffo/discussions");
+    newline();
+    println!("Copyright (c) 2023, junkben");
     line_break();
 }
